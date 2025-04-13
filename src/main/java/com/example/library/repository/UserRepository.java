d0477f8a858ebdbecb4a304d3f0147fba61741c4
Asiman77 <asiman.mirzaliyev2023@khazar.org>
2025-04-13 18:26:54 +0400

    Update UserRepository.java

--- src/main/java/com/example/library/repository/UserRepository.java
+++ src/main/java/com/example/library/repository/UserRepository.java
@@ -1,15 +1,4 @@
-/*package com.example.library.repository;
 
-import com.example.library.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    // Method to find a user by their name
-    User findByName(String name);
-    User findUserByNameAndRole(String username, String role);
-
-}
-*/
 
 package com.example.library.repository;
 
